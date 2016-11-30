75ed20e0df7d9fa59a674bbf1ad14bd52d660356
Simon Zolin <stsaz@yandex.ru>
2016-11-30 09:58:20 +0300

    + ffbit_find()

--- FF/bitops.h
+++ FF/bitops.h
@@ -12,6 +12,23 @@ Copyright (c) 2013 Simon Zolin
 #include <FF/bitops-vc.h>
 #endif
 
+#if !defined FF_MSVC
+#define ffbit_find32(n) \
+({ \
+	__typeof__(n) N = (n); \
+	(N == 0) ? 0 : __builtin_clz(N) + 1; \
+})
+
+#if defined FF_64
+#define ffbit_find64(n) \
+({ \
+	__typeof__(n) N = (n); \
+	(N == 0) ? 0 : __builtin_clzll(N) + 1; \
+})
+#endif
+
+#endif //!FF_MSVC
+
 #if defined FF_64
 /** bit: 0-63 */
 #define ffbit_test(/*size_t*/ i, bit)  ffbit_test64((uint64)(i), bit)
@@ -24,7 +41,11 @@ static FFINL ffbool ffbit_reset(size_t *p, uint bit) {
 	return ffbit_reset64((uint64*)p, bit);
 }
 
+/** Find the least significant 1-bit. */
 #define ffbit_ffs(/*size_t*/ i)  ffbit_ffs64((uint64)(i))
+
+/** Find the most significant 1-bit. */
+#define ffbit_find(/*size_t*/ i)  ffbit_find64((uint64)(i))
 
 #else //32 bit:
 /** bit: 0-31 */
@@ -65,6 +86,9 @@ static FFINL uint ffbit_ffs64(uint64 i) {
 		return ffbit_ffs((int)(i >> 32)) + 32;
 	return 0;
 }
+
+#define ffbit_find(/*size_t*/ i)  ffbit_find32((int)(i))
+
 #endif
 
 static FFINL ffbool ffbit_testarr(const uint *ar, uint bit) {
